61226b958b665d2cb7d71a0771af27d69c7e2b70
d-sharygin <d.sharygin@uni-eng.ru>
2026-05-28 09:22:40 +0300

    change settings

--- PMI-DZT2/meta_handler_inout.py
+++ PMI-DZT2/meta_handler_inout.py
@@ -13,12 +13,12 @@ config_handler = MainConfigHandler.from_json_file(meta_path)
 #t = config_handler.get_param_info("T_LVCBSUP_1_RCBF1_FuncEnabled")
 #print(t) lvrbvtr1
 
-pars = config_handler.find_parameters_starting_with("Sign")
+pars = config_handler.find_parameters_starting_with("DZT2")
 
 result = []
 for par in pars:
     if par["group"] == "Measurement" or par["group"] == "measurement": # "measurement":  #"setting": # and par["fullDescription"] == "Внешнее отключение от УРОВ НН":
-        print(par)
+        #print(par)
         result.append(par)  # добавляем в список       
     #if par["group"] == "Setting" or par["group"] == "setting":    
         #print(par)
@@ -27,3 +27,7 @@ for par in pars:
 # Сохранение всех параметров в out.json
 with open("out.json", "w", encoding="utf-8") as f:
     json.dump(result, f, ensure_ascii=False, indent=4)
+
+
+i = config_handler.get_param_info("DZT2_IO_1_VLVIsolOp")
+print(i)
